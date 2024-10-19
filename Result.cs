bb5b68424cee161d251c6f761e71c5eaf7fe9e9f
Deloar <deloarhosen2003@gmail.com>
2024-10-19 20:52:47 +0600

    Even-odd

---
+++ Result.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Result
+{
+static void Main()
+{
+   int num;
+   Console.Write($"Enter number:");
+try
+{
+   num=Convert.ToInt32(Console.ReadLine());
+  if(num%2==0)
+  {
+    Console.WriteLine($"number is even.");
+  }
+  else
+  {
+    Console.WriteLine($"number is odd.");
+    
+  }
+}
+catch(Exception e)
+{
+    Console.WriteLine($"Enter a valid number and the exeption is {e}");  
+}
+
+}
+}
+
+
+
